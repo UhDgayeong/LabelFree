5c9361897adb91023b6628ba7d7c08e4801c812d
UhDgayeong <gygy3686@naver.com>
2022-01-22 22:14:10 +0900

    change icon image

--- app/src/main/java/com/example/labelfree/SplashActivity.kt
+++ app/src/main/java/com/example/labelfree/SplashActivity.kt
@@ -11,6 +11,7 @@ import android.view.WindowManager
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
